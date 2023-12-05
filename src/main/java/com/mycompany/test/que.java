6b2a5e88f094a5db2eb27e3fe48631a659818ef8
janidu <jchamoth12@gmail.com>
2023-12-05 12:20:09 -0800

    all  method are defined

--- src/main/java/com/mycompany/test/que.java
+++ src/main/java/com/mycompany/test/que.java
@@ -9,5 +9,68 @@ package com.mycompany.test;
  * @author jcham
  */
 public class que {
-    
-}
+     int size = 5;
+     int items []=new int[size];
+     int front , rear;
+     
+     que(){
+         front = -1;
+         rear =-1;
+     }
+     boolean isfull(){
+         if(front==0&&rear==size-1){ // When the front and rear pointers meet, it means that the queue is full.
+             return true;
+         }
+         return false;
+     }
+         
+      boolean isempty(){
+         if (front==-1){
+             return true;
+         }
+          return false;
+         }
+      void enque(int element){
+           if(isfull()){
+               System.out.println("que is full");
+           }else{
+               if(front==-1){
+                   front=0;
+                   rear++;
+                   items[rear]= element;
+                   System.out.println("inserted"+element);
+                           
+               }
+           }       
+      }
+       int deque(){
+           int element;
+           if(isempty()){
+               System.out.println("que is empty");
+               return -1;
+           }else{
+               element = items[front];
+               if(front>=rear){
+                   front=-1;
+                   rear=-1;
+                   
+               }else{
+                   front++;
+                   
+               }
+               System.out.println("deleted"+element);
+           }
+              return element;
+         }
+             void display(){
+                    if (isempty()){
+                        System.out.println("que is empty");
+                    }
+                 for(int i =front;i<=rear;i++){
+                     System.out.println("fornt element are"+items[i]);
+                 }
+             }
+         
+  }
+     
+
